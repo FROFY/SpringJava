251a132d50b3da5fb9308e84b46a9b23cffc2a9a
FROFY <karos-76@yandex.ru>
2023-02-18 14:35:21 +0300

    init commit

---
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Task;
+import com.example.demo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+    @PostMapping("/tasks")
+    public Task create(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+    @GetMapping("/tasks")
+    public Iterable<Task> getAll() {
+        return taskRepository.findAll();
+    }
+}
---
+++ src/main/java/com/example/demo/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Task;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends PagingAndSortingRepository<Task, Long> {
+    Iterable<Task> findAll();
+    Task save(Task task);
+}
